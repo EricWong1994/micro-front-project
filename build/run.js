baa681f64fb3613c29df3cab0a7413b1eb02a121
王石浩 <wangshihao@wjbrain.com>
2022-10-27 18:21:52 +0800

    增加 npm install sass -D --legacy-peer-deps

--- build/run.js
+++ build/run.js
@@ -3,10 +3,11 @@ const path = require('path')
 
 const filePath = {
   service: path.join(__dirname, '../service'),
-  vue2: path.join(__dirname, '../vue2'),
+  // TODO 启动报错 * ./node_modules/vue-loader-v16/dist/templateLoader.js
+  // vue2: path.join(__dirname, '../vue2'),
   // todo 打开
   // vue3: path.join(__dirname, '../vue3'),
-  react15: path.join(__dirname, '../react15'),
+  // react15: path.join(__dirname, '../react15'),
   react16: path.join(__dirname, '../react16'),
   main: path.join(__dirname, '../main')
 }
@@ -36,11 +37,13 @@ function uninstallChildModules () {
 //   })
 // }
 
-function installChild () {
+function installsass () {
   Object.values(filePath).forEach(item => {
     childProcess.spawn(`cd ${item} && npm install sass -D --legacy-peer-deps`, { stdio: "inherit", shell: true })
   })
 }
 // installChild()
-// runChild()
-uninstallChildModules()
+// uninstallChildModules()
+// installsass()
+
+runChild()
--- main/src/components/Footer.vue
+++ main/src/components/Footer.vue
@@ -1,53 +1,53 @@
 <template>
-  <div class="main-footer-container">
-    <div class="main-footer-content">
-      <div class="main-footer-url">
-        <span>选车吗 © 2021 x.12345.com</span>
-        公司地址: 北京市海淀区花园路xx号xx大厦xx层xxxx室
-      </div>
-      <div class="main-footer-phone">
-        <span>联系方式: 010-00000000</span>
-        <span>京ICP备88888888号</span>
-      </div>
-    </div>
-  </div>
+	<div class="main-footer-container">
+		<div class="main-footer-content">
+			<div class="main-footer-url">
+				<span>选车吗 © 2021 x.12345.com</span>
+				公司地址: 北京市海淀区花园路xx号xx大厦xx层xxxx室
+			</div>
+			<div class="main-footer-phone">
+				<span>联系方式: 010-00000000</span>
+				<span>京ICP备88888888号</span>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-  name: 'index',
+	name: 'index',
 };
 </script>
 
 <style lang="scss">
-.main-footer{
-  &-container{
-    width: 100%;
-    height: 100px;
-    background: #F5F5F5;
-  }
-  &-content{
-    width: 1200px;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  &-url{
-    font-size: 14px;
-    color: #9E9E9E;
-    margin-bottom: 10px;
+.main-footer {
+	&-container {
+		width: 100%;
+		height: 100px;
+		background: #f5f5f5;
+	}
+	&-content {
+		width: 1200px;
+		height: 100%;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	&-url {
+		font-size: 14px;
+		color: #9e9e9e;
+		margin-bottom: 10px;
 
-    span{
-      font-size: 16px;
-      color: #A1A1A1;
-    }
-  }
-  &-phone{
-    font-size: 14px;
-    color: #9E9E9E;
-  }
+		span {
+			font-size: 16px;
+			color: #a1a1a1;
+		}
+	}
+	&-phone {
+		font-size: 14px;
+		color: #9e9e9e;
+	}
 }
 </style>
